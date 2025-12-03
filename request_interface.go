fed837a57fb2b88e9004cd70aa76b9dedffa638c
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2025-12-03 20:56:26 +0700

    docs: add comments for each method

--- request_interface.go
+++ request_interface.go
@@ -1,11 +1,15 @@
 package beatles
 
 type Request interface {
+	// Returns the value of a header by its key
 	GetHeader(key string) string
 
+	// Returns the HTTP method of the request
 	Method() string
 
+	// Returns the request path
 	Path() string
 
+	// Returns the value of a path parameter by key
 	PathVariable(key string) string
 }
--- response_interface.go
+++ response_interface.go
@@ -1,11 +1,15 @@
 package beatles
 
 type Response interface {
+	// Sets or updates a response header by key and value
 	SetHeader(key string, value string) Response
 
+	// Sets HTTP status code for the response
 	SetStatus(statusCode int) Response
 
+	// Sets the raw response body as a byte slice
 	SetBody(body []byte) error
 
+	// Serializes the given value into JSON and writes it to the response body
 	JSON(v JSON) error
 }
